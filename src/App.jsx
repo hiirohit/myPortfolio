60da841839012d0d188b825c71ebe7ed003da99f
Rohit Kumar <kumarrohit41175@gmail.com>
2025-10-29 21:46:09 +0530

    remove blurEffect

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import BlurEffect from "./blurEffect";
 const App = () => {
   return (
     <div className="bg-[#050414]">
-      <BlurEffect position={{top:"65%", left:"20%" }} size ={{width:"30%",height:"40%"}}></BlurEffect>
+      
       <div className="absolute inset-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-size-[14px_24px] mask-[radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]">  
       </div>
       
